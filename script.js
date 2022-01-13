bb51a7163f888adf0e5dfa1b03bd24a7e528c943
KHUSHI9695 <97609841+KHUSHI9695@users.noreply.github.com>
2022-01-13 19:32:49 +0530

    Login Page
    for using HTML , CSS, Javascript i Have making a login page

---
+++ script.js
@@ -0,0 +1,17 @@
+function validation()
+{
+     
+    var username= document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+    if(username=="admin"&& password=="user")
+    {
+        alert("Login Successfully");
+        return false;
+    }
+    else
+    {
+        alert("Login Failed");
+    }
+
+
+}
